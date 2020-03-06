cd39ace0a2aa6e8c9aa2b220f18df6f96f43423a
WagnerSolak <wwwagnerww@gmail.com>
2020-03-05 21:16:52 -0300

    Projeto criado

---
+++ src/main/java/com/example/solak/api/FinanceiroApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.solak.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication  // aplicação co spring boot que aborda várias outras
+public class FinanceiroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceiroApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/solak/api/FinanceiroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.solak.api;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceiroApiApplicationTests {
+
+	
+	void contextLoads() {
+	}
+
+}

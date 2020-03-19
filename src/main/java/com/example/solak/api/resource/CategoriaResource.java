899abd7f7583b2d5ba6edc05d1d32b425e1e7ebd
WagnerSolak <wwwagnerww@gmail.com>
2020-03-18 21:50:54 -0300

    Salvando categoria com POST

--- src/main/java/com/example/solak/api/resource/CategoriaResource.java
+++ src/main/java/com/example/solak/api/resource/CategoriaResource.java
@@ -1,11 +1,21 @@
 package com.example.solak.api.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.solak.api.model.Categoria;
 import com.example.solak.api.repository.CategoriaRepository;
@@ -21,4 +31,25 @@ public class CategoriaResource { //recurso de categoria
 	public List<Categoria> listar(){
 		return categoriaRepository.findAll();
 	}
+	
+	@PostMapping
+	//@ResponseStatus(HttpStatus.CREATED) não preciso mais da anotação, porque o created(uri) já define o status 
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response){
+		Categoria cataegoriaSalva = categoriaRepository.save(categoria);
+		
+		//Código para o header informar em Location o value do post efetuado
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")  // a partir da requisição atual add o codigo
+		.buildAndExpand(cataegoriaSalva.getCodigo()).toUri(); // adicionando o codigo na uri
+		response.setHeader("Location", uri.toASCIIString());  // setando o headerLocation na uri
+		
+		return ResponseEntity.created(uri).body(cataegoriaSalva);
+	}
+	
+	@GetMapping("/{codigo}")
+	public Categoria buscarPorCodigo(@PathVariable Long codigo){
+		return categoriaRepository.findOne(codigo);
+	}
+	
+	
+	
 }

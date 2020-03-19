2256f76a57a407ddcf0790178659fdd64e6b0f6b
WagnerSolak <wwwagnerww@gmail.com>
2020-03-19 17:59:43 -0300

    Deletando pessoa com DELETE

--- src/main/java/com/example/solak/api/resource/PessoaResource.java
+++ src/main/java/com/example/solak/api/resource/PessoaResource.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,6 +52,12 @@ public class PessoaResource {
 		Pessoa pessoa = pessoaRepository.findOne(codigo);
 		
 		return pessoa != null ? ResponseEntity.ok().body(pessoa) : ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deletar(@PathVariable Long codigo){
+		pessoaRepository.delete(codigo);
 	}
 
 }

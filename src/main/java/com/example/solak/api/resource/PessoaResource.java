e8a0a0bb32528e8c34fe023609ffdf18b33360a3
WagnerSolak <wwwagnerww@gmail.com>
2020-03-25 14:00:36 -0300

    Atualizando pessoa com PUT

--- src/main/java/com/example/solak/api/resource/PessoaResource.java
+++ src/main/java/com/example/solak/api/resource/PessoaResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -21,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.solak.api.event.ResourceCreatedEvent;
 import com.example.solak.api.model.Pessoa;
 import com.example.solak.api.repository.PessoaRepository;
+import com.example.solak.api.service.PessoaService;
 
 @RestController
 @RequestMapping("/pessoas")
@@ -31,6 +34,9 @@ public class PessoaResource {
 	
 	@Autowired
 	private ApplicationEventPublisher publisher; //para poder disparar o evento
+	
+	@Autowired
+	private PessoaService pessoaService;
 	
 	@GetMapping
 	public List<Pessoa> listar(){
@@ -59,5 +65,11 @@ public class PessoaResource {
 	public void deletar(@PathVariable Long codigo){
 		pessoaRepository.delete(codigo);
 	}
-
+	
+	@PutMapping("/{codigo}")
+	public ResponseEntity<Pessoa> atualizar(@PathVariable Long codigo, @Valid @RequestBody Pessoa pessoa){
+		Pessoa pessoaSalva = pessoaService.atualizar(codigo, pessoa);
+		
+		return ResponseEntity.ok(pessoaSalva);
+	}
 }
---
+++ src/main/java/com/example/solak/api/service/PessoaService.java
@@ -0,0 +1,25 @@
+package com.example.solak.api.service;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.example.solak.api.model.Pessoa;
+import com.example.solak.api.repository.PessoaRepository;
+
+@Service
+public class PessoaService {
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	public Pessoa atualizar(Long codigo, Pessoa pessoa){
+		Pessoa pessoaSalva = pessoaRepository.findOne(codigo);
+		if(pessoaSalva == null){
+			throw new EmptyResultDataAccessException(1); //lança exceção (1) -> esperava ao menos 1 elemento, e assim retorna 404
+		}
+		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo"); // copia as propriedades, passando dequal objeto? para qual objeto? sem pegar o código, pois este é nulo!
+		return pessoaRepository.save(pessoaSalva);
+	}
+}

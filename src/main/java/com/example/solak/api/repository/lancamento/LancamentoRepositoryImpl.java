b0772decd244d34cf5c3573f5ebe4740c653f17a
WagnerSolak <wwwagnerww@gmail.com>
2020-04-02 12:49:48 -0300

    Paginação para consulta com GET em lancamento

--- src/main/java/com/example/solak/api/repository/lancamento/LancamentoRepositoryImpl.java
+++ src/main/java/com/example/solak/api/repository/lancamento/LancamentoRepositoryImpl.java
@@ -11,64 +11,90 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.util.StringUtils;
 
 import com.example.solak.api.model.Lancamento;
 import com.example.solak.api.model.Lancamento_;
 import com.example.solak.api.repository.filter.LancamentoFilter;
 
-public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery{
+public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery {
 
 	@PersistenceContext
 	private EntityManager manager;
 
 	@Override
-	public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter) {
+	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable) {
 		CriteriaBuilder builder = manager.getCriteriaBuilder();
 		CriteriaQuery<Lancamento> criteria = builder.createQuery(Lancamento.class);
 		Root<Lancamento> root = criteria.from(Lancamento.class);
-		
-		//restrictions
+
+		// restrictions
 		Predicate[] predicates = adicionarRestricoes(lancamentoFilter, builder, root);
 		criteria.where(predicates);
-		
+
 		TypedQuery<Lancamento> query = manager.createQuery(criteria);
-		return query.getResultList();
-		
+
+		// return query.getResultList();
+
+		adicionarDetalhesDaPaginacao(query, pageable);
+
+		return new PageImpl<>(query.getResultList(), pageable, total(lancamentoFilter));
 	}
+
+	
 
 	private Predicate[] adicionarRestricoes(LancamentoFilter lancamentoFilter, CriteriaBuilder builder,
 			Root<Lancamento> root) {
 		List<Predicate> predicates = new ArrayList<>();
-		
-		if(!StringUtils.isEmpty(lancamentoFilter.getDescricao())){
+
+		if (!StringUtils.isEmpty(lancamentoFilter.getDescricao())) {
 			predicates.add(builder.like(builder.lower(root.get(Lancamento_.descricao)),
-					"%" + lancamentoFilter.getDescricao().toLowerCase()
-						+ "%"));
+					"%" + lancamentoFilter.getDescricao().toLowerCase() + "%"));
 		}
-		
-		if(lancamentoFilter.getDataVencimentoDe() != null){
+
+		if (lancamentoFilter.getDataVencimentoDe() != null) {
 			predicates.add(builder.greaterThanOrEqualTo(root.get(Lancamento_.dataVencimento),
 					lancamentoFilter.getDataVencimentoDe()));
 		}
-		
-		if(lancamentoFilter.getDataVencimentoAte() != null){
+
+		if (lancamentoFilter.getDataVencimentoAte() != null) {
 			predicates.add(builder.lessThanOrEqualTo(root.get(Lancamento_.dataVencimento),
 					lancamentoFilter.getDataVencimentoAte()));
 		}
-		
-		if(lancamentoFilter.getValorDe() != null){
-			predicates.add(builder.greaterThanOrEqualTo(root.get(Lancamento_.valor),
-					lancamentoFilter.getValorDe()));
+
+		if (lancamentoFilter.getValorDe() != null) {
+			predicates.add(builder.greaterThanOrEqualTo(root.get(Lancamento_.valor), lancamentoFilter.getValorDe()));
 		}
-		
-		if(lancamentoFilter.getDataVencimentoAte() != null){
-			predicates.add(builder.lessThanOrEqualTo(root.get(Lancamento_.valor),
-					lancamentoFilter.getValorAte()));
+
+		if (lancamentoFilter.getDataVencimentoAte() != null) {
+			predicates.add(builder.lessThanOrEqualTo(root.get(Lancamento_.valor), lancamentoFilter.getValorAte()));
 		}
-		
+
 		return predicates.toArray(new Predicate[predicates.size()]);
 	}
+
+	private void adicionarDetalhesDaPaginacao(TypedQuery<Lancamento> query, Pageable pageable) {
+		int paginaAtual = pageable.getPageNumber();
+		int totalRegistroPorPagina = pageable.getPageSize();
+		int primeiroRegistroDaPagina = paginaAtual * totalRegistroPorPagina;
+		
+		query.setFirstResult(primeiroRegistroDaPagina);
+		query.setMaxResults(totalRegistroPorPagina);
+	}
 	
-	
+	private Long total(LancamentoFilter lancamentoFilter) {
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
+		CriteriaQuery<Long> criteria = builder.createQuery(Long.class);
+		Root<Lancamento> root = criteria.from(Lancamento.class); // from Lancamento
+		
+		Predicate[] predicates = adicionarRestricoes(lancamentoFilter, builder, root);
+		criteria.where(predicates); // where restrictions
+		
+		criteria.select(builder.count(root));  // builder.count(root) -> contador 
+		return manager.createQuery(criteria).getSingleResult();
+	}
+
 }
--- src/main/java/com/example/solak/api/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/com/example/solak/api/repository/lancamento/LancamentoRepositoryQuery.java
@@ -1,11 +1,12 @@
 package com.example.solak.api.repository.lancamento;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import com.example.solak.api.model.Lancamento;
 import com.example.solak.api.repository.filter.LancamentoFilter;
 
 public interface LancamentoRepositoryQuery {
 
-	public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
+	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 }
